dac9bb0fa2d5e016c0fdb737b93ceddcb0cc874f
Neel Patel <neelp8501@gmail.com>
2021-05-24 22:31:59 +0530

    find book on internet

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import axios from 'axios'
+import { useState, useRef, useCallback } from 'react'
+import useBookSearch from './useBookSearch'
+
+function App() {
+  const [query, setQuery] = useState('')
+  const [pageNumber, setPageNumber] = useState(1)
+  const {
+    loading,
+    error,
+    books,
+    hasMore
+  } = useBookSearch(query, pageNumber)
+
+  const observer = useRef()
+  const lastBookElementRef = useCallback(node => {
+    if (loading) return
+    if (observer.current) observer.current.disconnect()
+    observer.current = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting && hasMore) {
+        setPageNumber(prevPageNumber => prevPageNumber + 1)
+      }
+    })
+    if (node) observer.current.observe(node)
+    // console.log(node);
+  }, [loading, hasMore])
+
+  function handleSearch(e) {
+    setQuery(e.target.value)
+    setPageNumber(1)
+  }
+
+  return (
+    <>
+      <input type="text" value={query} onChange={handleSearch}></input>
+      {books.map((book, index) => {
+        if (books.length === index + 1) {
+          return <div ref={lastBookElementRef} key={book}>{book}</div>
+        }
+        else {
+          return <div key={book}>{book}</div>
+        }
+      })}
+      <p><br />{loading && 'Loading...'}</p>
+      <p><br />{error && 'ERROR'}</p>
+    </>
+  );
+}
+
+export default App;
